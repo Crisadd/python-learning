7c5624f80f0b337fba412f4bdbd257cb6ce4b987
Cristian <cristianarieldelgado@gmail.com>
2026-07-09 10:07:56 -0300

    asd

--- HarvardX-CS50P/week03 - Exceptions/number.py
+++ HarvardX-CS50P/week03 - Exceptions/number.py
@@ -1,4 +1,4 @@
-while True:
+""" while True:
     try: 
         x = int(input("What's x? "))
 
@@ -11,3 +11,11 @@ while True:
 print(f'x is {x}')
 
 TERMINE EN 27:14
+
+"""
+
+string = """My name is Cristian Delgado. I am a commercial pilot with a true passion for aviation. I have the privilege of flying passengers to their destinations, a responsibility I embrace with professionalism, commitment, and enthusiasm.
+I enjoy flying, discovering new countries, experiencing different cultures, and exploring new destinations that allow me to continue growing both professionally and personally. I believe in continuous learning, teamwork, and maintaining the highest standards of safety a
+"""
+
+print(len(string))
